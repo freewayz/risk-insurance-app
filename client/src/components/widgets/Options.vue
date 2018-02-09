f33c5900fbdbec2c80eb2a5041769fc6c0e6e15e
peter <fr33wayz@gmail.com>
2018-02-09 02:42:44 +0100

    update risk page

--- client/src/components/widgets/Options.vue
+++ client/src/components/widgets/Options.vue
@@ -1,8 +1,13 @@
 <template>
     <div class="form-group">
         <label for=""> {{ field.label }}</label>
-        <div v-for="(op, index) in field.options" :key="index">
-            <input type="radio" :name="field.label">
+        <div class="col-sm-12">
+            <div class="form-check" v-for="(opt, index) in field.options" :key="index">
+            <input class="form-check-input" :id="index + opt.label"  type="radio" :name="field.label">
+            <label :for="index + opt.label" class="form-check-label">
+                {{ opt.label }}
+            </label>
+        </div>
         </div>
     </div>
 </template>
