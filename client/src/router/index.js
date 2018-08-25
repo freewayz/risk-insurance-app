dc53ea1679c0423b30c6bed7252e1de643d3719a
peter <fr33wayz@gmail.com>
2018-08-25 19:44:28 +0100

    add login and registration path to routes

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -4,10 +4,22 @@ import Risk from '@/components/Risk'
 import RiskForm from '@/components/RiskForm'
 import RiskFormBuilder from '@/components/builder/RiskFormBuilder'
 import CreateRisk from '@/components/CreateRisk'
+import Login from '@/components/auth/Login'
+import Register from '@/components/auth/Register'
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: Register
+    },
     {
       path: '/',
       name: 'Risk',
