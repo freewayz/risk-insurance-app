fc1fa01b115b37feb4e01dc22df18c4cda61c650
peter <fr33wayz@gmail.com>
2018-08-25 19:44:40 +0100

    feat: added login component

---
+++ client/src/components/auth/Login.vue
@@ -0,0 +1,33 @@
+<template>
+  <el-container>
+    <el-form size="mini">
+      <el-form-item>
+        <el-input v-model="model.username"/>
+      </el-form-item>
+      <el-form-item>
+        <el-input v-model="model.password">
+      </el-form-item>
+    </el-form>
+  </el-container>
+
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+      model: {
+        username: '',
+        password: ''
+      }
+    }
+  },
+
+  methods: {
+    onLogin () {
+
+    }
+  }
+}
+</script>
