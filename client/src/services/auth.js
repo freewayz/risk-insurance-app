c7706b957cd1645b068f850130b04cdc02824f1c
peter <fr33wayz@gmail.com>
2018-08-25 20:17:35 +0100

    added auth.js service file

---
+++ client/src/services/auth.js
@@ -0,0 +1,7 @@
+import http from '@utils/http'
+
+const LOGIN_URL = '/account/login/'
+const REGISTER_URL = '/account/register/'
+
+export const login = (data) => http.post(LOGIN_URL, data)
+export const register = (data) => http.post(REGISTER_URL, data)
